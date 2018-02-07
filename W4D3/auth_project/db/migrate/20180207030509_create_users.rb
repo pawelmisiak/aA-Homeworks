fbeefa937f3a8edafbde3c93de140bcd95b8aa3a
pawel misiak <pawelmisiak@live.com>
2018-02-06 22:36:03 -0500

    Work on Auth_project

---
+++ W4D3/auth_project/db/migrate/20180207030509_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      #username:string password_digest:string session_token:string
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
